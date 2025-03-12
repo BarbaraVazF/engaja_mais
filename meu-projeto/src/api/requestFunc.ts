3012b18dac6d0b729fa4a4e544ac4062d39ae660
Bárbara Vaz Ferreira <barbara.ferreira@neogrid.com>
2025-03-12 15:29:43 -0300

    ...

---
+++ meu-projeto/src/api/requestFunc.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const BACKEND_URL = "http://localhost:3000";
+
+export async function requestFunc(content) {
+    try {
+        const response = await axios.post(`${BACKEND_URL}/api/student`, content, { // substituir student
+            withCredentials: true,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        return response.data;
+    } catch (error) {
+        console.error("Erro ao gerar conteudo:", error);
+        return { error: "Erro ao gerar conteudo." };
+    }
+}
