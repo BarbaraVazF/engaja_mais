97a55bd8aaf64eafe89ab37fe01e09d75c5c0888
Sidney Alex <silvercenturion2@gmail.com>
2025-04-08 21:49:57 -0300

    ajusta proxy

--- meu-projeto/src/components/UserPopup.jsx
+++ meu-projeto/src/components/UserPopup.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import { authClient } from "../lib/auth-client"; // Importa o authClient
 
 const UserPopup = ({ onClose }) => {
@@ -13,21 +13,55 @@ const UserPopup = ({ onClose }) => {
     return <div>Carregando...</div>;
   }
 
-  const { name, cpf, email } = session.user; // Pegando os dados do usuário
+  const { name, email } = session.user; // Pegando os dados do usuário
 
   const handleLogout = async () => {
-    await authClient.signOut({ fetchOptions: { onSuccess: () => { navigate('/login') } } });
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          navigate("/login");
+        },
+      },
+    });
   };
 
   return (
     <div className="popup-overlay" onClick={onClose}>
       <div className="popup" onClick={(e) => e.stopPropagation()}>
         <h2>Dados do Usuário</h2>
-        <p><strong>Nome:</strong> {name}</p>
-        <p><strong>Email:</strong> {email}</p>
+        <p>
+          <strong>Nome:</strong> {name}
+        </p>
+        <p>
+          <strong>Email:</strong> {email}
+        </p>
         <div className="popup-buttons">
-          <button onClick={onClose} style={{ backgroundColor: '#5A5858', color: 'white', width: '100px', height: '40px', marginTop: '10px', marginLeft: '10%' }}>Fechar</button>
-          <button onClick={handleLogout} style={{ backgroundColor: '#510202', color: 'white', width: '100px', height: '40px', marginTop: '10px', marginRight: '10%' }}>Sair</button>
+          <button
+            onClick={onClose}
+            style={{
+              backgroundColor: "#5A5858",
+              color: "white",
+              width: "100px",
+              height: "40px",
+              marginTop: "10px",
+              marginLeft: "10%",
+            }}
+          >
+            Fechar
+          </button>
+          <button
+            onClick={handleLogout}
+            style={{
+              backgroundColor: "#510202",
+              color: "white",
+              width: "100px",
+              height: "40px",
+              marginTop: "10px",
+              marginRight: "10%",
+            }}
+          >
+            Sair
+          </button>
         </div>
       </div>
     </div>
