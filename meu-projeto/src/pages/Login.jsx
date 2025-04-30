83fc520a754fd2316844c49f778272c0f4cda673
Bárbara Vaz Ferreira <barbara.ferreira@neogrid.com>
2025-04-30 11:13:02 -0300

    ...

--- meu-projeto/src/pages/Login.jsx
+++ meu-projeto/src/pages/Login.jsx
@@ -70,6 +70,7 @@ const Login = () => {
           boxShadow: "0 2px 6px rgba(0,0,0,0.1)",
           width: "500px", // menor largura para melhor centralização
           maxWidth: "90%",
+          marginTop: "-10px",
         }}
       >
         <img
